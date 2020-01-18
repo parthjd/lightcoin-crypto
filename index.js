d508269c57ace9154d88de15e564a694645bd0f2
parthjd <parthjd28@gmail.com>
2020-01-18 23:51:26 +0000

    LHL Crypto with edge cases but with error

--- index.js
+++ index.js
@@ -1,29 +1,79 @@
-let balance = 500.00;
+// let balance = 500.0;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
 
-  constructor(amount) {
+  addTransactions(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
+}
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    // note how it has access to this.account b/c of parent
+    return this.account.balance - this.amount >= 0;
+  }
+}
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
+  }
 }
 
-
-
-
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+// NEW ACCOUNT
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+const myAccount = new Account();
 
-console.log('Balance:', balance);
+console.log("Starting Account Balance: ", myAccount.balance);
+
+console.log("Attempting to withdraw even $1 should fail...");
+const t1 = new Withdrawal(1.0, myAccount);
+console.log("Commit result:", t1.commit());
+console.log("Account Balance: ", myAccount.balance);
+
+console.log("Depositing should succeed...");
+const t2 = new Deposit(9.99, myAccount);
+console.log("Commit result:", t2.commit());
+console.log("Account Balance: ", myAccount.balance);
+
+console.log("Withdrawal for 9.99 should be allowed...");
+const t3 = new Withdrawal(9.99, myAccount);
+console.log("Commit result:", t3.commit());
+
+console.log("Ending Account Balance: ", myAccount.balance);
+console.log("Looking like I'm broke again");
+
+console.log("Account Transaction History: ", myAccount.transactions);
